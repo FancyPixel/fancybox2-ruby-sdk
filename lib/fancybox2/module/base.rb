ac412d7a21a3365b98340ea633c7a9165af55b7c
Alessandro Verlato <alessandro@fancypixel.it>
2020-04-06 16:51:52 +0200

    On Module Base

--- lib/fancybox2/module/base.rb
+++ lib/fancybox2/module/base.rb
@@ -1,26 +1,69 @@
+require 'logger'
+require 'paho-mqtt'
+
 module Fancybox2
   module Module
     class Base
       include Utils
 
-      attr_accessor :logger, :log_level, :log_progname
+      attr_accessor :logger, :log_level, :log_progname, :mqtt_client
 
-      def initialize(options = {})
-        options.deep_symbolize_keys!
+      def initialize(*args)
+        options = args.extract_options.deep_symbolize_keys!
+        @mqtt_client = options[:mqtt_client]
+        @mqtt_client_params = options[:mqtt_client_params] || {}
         @log_level = options[:log_level] || ::Logger::DEBUG
         @log_progname = options[:log_progname] || 'Fancybox2::Module::Base'
-        @logger = options[:logger] || build_default_logger
+        @logger = options[:logger]
+      end
+
+      def run
+        mqtt_client.connect
+      end
+
+      def logger
+        return @logger if @logger
+        stdout_logger = ::Logger.new STDOUT
+        broker_logger = ::Logger.new(Logger::MQTTLogDevice.new(client: mqtt_client), formatter: Logger::JSONFormatter.new)
+        @logger = Logger::Multi.new stdout_logger, broker_logger,
+                                         level: @log_level,
+                                         progname: @log_progname,
+                                         escape_data: true
+      end
+
+      def mqtt_client
+        return @mqtt_client if @mqtt_client
+        @mqtt_client = PahoMqtt::Client. new mqtt_params
+        @mqtt_client
       end
 
       private
 
-      def build_default_logger
-        stdout_logger = ::Logger.new STDOUT
-        multi_logger = Logger::Multi.new stdout_logger,
-                                         level: @log_level,
-                                         progname: @log_progname,
-                                         escape_data: true
-        multi_logger
+      def mqtt_default_params
+        {
+            host:           'localhost',
+            port:           1883,
+            mqtt_version:   '3.1.1',
+            clean_session:  true,
+            persistent:     false,
+            blocking:       true,
+            client_id:      nil,
+            username:       nil,
+            password:       nil,
+            ssl:            false,
+            will_topic:     nil,
+            will_payload:   nil,
+            will_qos:       0,
+            will_retain:    false,
+            keep_alive:     10,
+            ack_timeout:    5
+        }
+      end
+
+      def mqtt_params
+        return @mqtt_params if @mqtt_params
+        @mqtt_params = mqtt_default_params.merge(@mqtt_client_params) { |key, old_val, new_val| new_val.nil? ? old_val : new_val }
+        @mqtt_params
       end
     end
   end
--- spec/fancybox2/module/base_spec.rb
+++ spec/fancybox2/module/base_spec.rb
@@ -7,4 +7,13 @@ describe Fancybox2::Module::Base do
 
     it { should have_attr_reader :logger }
   end
+
+  describe 'initialize' do
+    let(:module_base) { Fancybox2::Module::Base.new }
+
+    it "is expected to set the logger" do
+      expect(module_base.logger).to_not be_nil
+    end
+
+  end
 end
