c09a98c1a33882d9016a48b51f71699ee9ab8c36
Alessandro Verlato <alessandro@fancypixel.it>
2020-10-28 11:50:25 +0100

    Add missing instance variable set

--- lib/fancybox2/utils/os.rb
+++ lib/fancybox2/utils/os.rb
@@ -7,31 +7,31 @@ module Fancybox2
         return @identifier if @identifier
 
         host_os = RbConfig::CONFIG['host_os']
-        case host_os
-        when /aix(.+)$/
-          'aix'
-        when /darwin(.+)$/
-          'darwin'
-        when /linux/
-          'linux'
-        when /freebsd(.+)$/
-          'freebsd'
-        when /openbsd(.+)$/
-          'openbsd'
-        when /netbsd(.*)$/
-          'netbsd'
-        when /dragonfly(.*)$/
-          'dragonflybsd'
-        when /solaris2/
-          'solaris2'
-        when /mswin|mingw32|windows/
-          # No Windows platform exists that was not based on the Windows_NT kernel,
-          # so 'windows' refers to all platforms built upon the Windows_NT kernel and
-          # have access to win32 or win64 subsystems.
-          'windows'
-        else
-          host_os
-        end
+        @identifier = case host_os
+                      when /aix(.+)$/
+                        'aix'
+                      when /darwin(.+)$/
+                        'darwin'
+                      when /linux/
+                        'linux'
+                      when /freebsd(.+)$/
+                        'freebsd'
+                      when /openbsd(.+)$/
+                        'openbsd'
+                      when /netbsd(.*)$/
+                        'netbsd'
+                      when /dragonfly(.*)$/
+                        'dragonflybsd'
+                      when /solaris2/
+                        'solaris2'
+                      when /mswin|mingw32|windows/
+                        # No Windows platform exists that was not based on the Windows_NT kernel,
+                        # so 'windows' refers to all platforms built upon the Windows_NT kernel and
+                        # have access to win32 or win64 subsystems.
+                        'windows'
+                      else
+                        host_os
+                      end
       end
     end
   end
